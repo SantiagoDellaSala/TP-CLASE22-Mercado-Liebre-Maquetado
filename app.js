50dcfe4ac1463be044ee0c17183ee2f89ebb9ca3
Santiago <dellasala1992@gmail.com>
2024-04-18 09:00:32 -0300

    Agregando estilos

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3000;
+const methodOverride = require('method-override');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
 app.get('/products', (req, res) => res.sendFile(path.join(__dirname, 'views', 'products.html')));
 app.get('/users', (req, res) => res.sendFile(path.join(__dirname, 'views', 'users.html')));
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'views', 'notFound.html')));
 
-app.listen(PORT, () => console.log('Servidor corriendo en el puerto: ' + PORT));
+app.listen(PORT, () => console.log(`Server running in: http://localhost:${PORT}`));
