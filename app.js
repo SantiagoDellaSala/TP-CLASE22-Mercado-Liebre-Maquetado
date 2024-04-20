4a9a4831e949d56d080d6722b42729449d204e0a
Santiago <dellasala1992@gmail.com>
2024-04-20 16:00:15 -0300

    Empezando CRUD

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 const methodOverride = require('method-override');
+const indexRouter = require('./routes/index.router');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/', indexRouter);
 app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
 app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'views', 'notFound.html')));
