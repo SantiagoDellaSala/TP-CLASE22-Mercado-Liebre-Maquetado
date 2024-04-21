1c73b08aac279ece0cd7b2307550b6189bee8cad
Santiago <dellasala1992@gmail.com>
2024-04-21 20:35:43 -0300

    a

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const indexController = require('../controllers/indexController')
 
-router.get('/', indexController.index);
+router.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, '../', 'views', 'home.html'))
+});
 
 module.exports = router;
